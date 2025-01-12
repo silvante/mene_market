bcef7e1a4480d8bc5dd31220abf489005feb51be
silvante <khamidov.ko@gmail.com>
2025-01-13 00:40:18 +0500

    new origin

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(helmet());
 
 // cors
 
-const allowedOrigins = [process.env.ORIGIN1, process.env.ORIGIN2];
+const allowedOrigins = [process.env.ORIGIN1, process.env.ORIGIN2, process.env.ORIGIN3];
 
 app.use(
   cors({
