f355400c6110889e748573fa95aba5b41d87ab50
silvante <khamidov.ko@gmail.com>
2025-01-14 16:22:25 +0500

    payments :: swagger

--- seeds.js
+++ seeds.js
@@ -8,13 +8,13 @@ const cyfer = bcryptjs.genSaltSync(8);
 const seedUsers = async () => {
     try {
         const owner = await User.findOne({status: "owner"})
-        if (owner) {
+        if (!owner) {
             console.log("Owners already exist!");
         } else {
             await User.deleteMany()
             const users = [
                 { name: "Owner of Market", username: "owner", email: "owner@mmarket.com", password: bcryptjs.hashSync(process.env.OWNER_PASSWORD, cyfer), verificated: true, status: "owner" },
-                { name: "Assembler", username: "assembler", email: "asm@we.com", password: bcryptjs.hashSync(process.env.OWNER_PASSWORD, cyfer), verificated: true, status: "owner" }
+                { name: "Assembler", username: "assembler", email: "newassembler@hey.com", password: bcryptjs.hashSync(process.env.OWNER_PASSWORD, cyfer), verificated: true, status: "owner" }
             ]
             await User.insertMany(users)
             console.log("Seeded users successfully!");
