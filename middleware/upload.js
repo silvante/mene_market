9d86e4fbec13b95ca69793e24e23349795a97f8d
silvante <khamidov.ko@gmail.com>
2025-02-15 15:43:34 +0500

    limit to 50mb

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
 const storage = multer.memoryStorage(); // Storing files in memory (for buffer access)
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fileSize: 10 * 1024 * 1024 } });
 
-module.exports = upload
+module.exports = upload;
