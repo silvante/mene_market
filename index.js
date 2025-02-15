2913d78ef7d06bd41613c9afd4901a70cc4b7e12
silvante <khamidov.ko@gmail.com>
2025-02-15 15:38:34 +0500

    limit to 50mb

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const Category = require("./models/category.model.js");
 const Product = require("./models/product.model.js");
 
 app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // for swagger
 
