465df61bde11835b9fbba5cbad98d292d7512ca2
silvante <khamidov.ko@gmail.com>
2025-03-01 13:56:19 +0500

    messager

---
+++ messangers/send_message.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const { LoadToken } = require("./token_loader");
+
+const SendMessange = async (chat_id, message) => {
+  try {
+    const bot_token = await LoadToken();
+    if (!bot_token) console.log("❌ token loading error");
+
+    let rendered_message = `
+    menemarket sayti tomonidan:
+
+    habar: ${message}
+
+    sahifa: menemarket.uz ✅
+    `;
+
+    const response = await axios.post(
+      `https://api.telegram.org/bot${bot_token}/sendMessage`,
+      { chat_id: chat_id, text: rendered_message }
+    );
+
+    console.log(`✅ Telegram Response: ${response.data}`);
+  } catch (err) {
+    console.log(`error: ${err.message}`);
+  }
+};
+
+module.exports = SendMessange;
---
+++ messangers/token_loader.js
@@ -0,0 +1,19 @@
+const TokenKeeper = require("./keeper/keeper.model");
+
+let bot_token = null;
+
+const LoadToken = async () => {
+  if (bot_token) return bot_token;
+  try {
+    const bot_data = await TokenKeeper.findOne();
+    if (!bot_data) {
+      throw new Error("Token is not active");
+    }
+    bot_token = bot_data.bot_token;
+    console.log("✅ Bot token loaded successfully!");
+  } catch (err) {
+    console.log(`❌ loading error: ${err.message}`);
+  }
+};
+
+module.exports = { LoadToken };
